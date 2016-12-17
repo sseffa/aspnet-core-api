2a0374d9a3d40b12228c7a69c50c8a63b248d951
sseffa <karagozsefa@gmail.com>
2016-12-18 01:07:55 +0200

    formatters and content negatiation

--- src/CityInfo/Startup.cs
+++ src/CityInfo/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
@@ -17,7 +18,10 @@ namespace CityInfo
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()
+                    ));
                 //.AddJsonOptions(o => {
 
                 //    if (o.SerializerSettings.ContractResolver != null)
