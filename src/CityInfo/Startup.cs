05c9f527b48885d71e044ee75bb18fe2639255d8
sseffa <karagozsefa@gmail.com>
2016-12-18 00:47:40 +0200

    working with serializer settings

--- src/CityInfo/Startup.cs
+++ src/CityInfo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo
 {
@@ -16,8 +17,18 @@ namespace CityInfo
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore()
-                .AddJsonFormatters();
+            services.AddMvc();
+                //.AddJsonOptions(o => {
+
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver
+                //            as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                    
+                //    }
+                        
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
