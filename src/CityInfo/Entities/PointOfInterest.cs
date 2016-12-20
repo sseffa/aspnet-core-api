88d252c8e0754140b0999c61623bf94c9917d6f9
sseffa <karagozsefa@gmail.com>
2016-12-21 00:44:12 +0200

    creating entity classes

---
+++ src/CityInfo/Entities/PointOfInterest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City city { get; set; }
+        public int cityId { get; set; }
+    }
+}
