170b0cd8b065f8f978b1e65432292445d4f5ff35
sseffa <karagozsefa@gmail.com>
2017-01-17 11:42:56 +0200

    seeding with database with data

---
+++ src/CityInfo/CityInfoExtensions.cs
@@ -0,0 +1,45 @@
+﻿using CityInfo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo
+{
+    public static class CityInfoExtensions
+    {
+        public static void EnsureSeedDataForContext(this CityInfoContext context)
+        {
+            if (context.Cities.Any())
+            {
+                return;
+            }
+
+            var cities = new List<City>() {
+
+                new City()
+                {
+
+                    Name = "New York City",
+                    Description = "Lorem ipsum sit amet",
+                    PointsOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                                Name = "Central Park",
+                                Description = "Lorem ipsum sit amet",
+                        },
+                        new PointOfInterest()
+                        {
+                                Name = "Empire State Building",
+                                Description = "Lorem ipsum sit amet",
+                        }
+                    }
+                }
+           };
+
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
+        }
+    }
+}
--- src/CityInfo/Startup.cs
+++ src/CityInfo/Startup.cs
@@ -63,7 +63,7 @@ namespace CityInfo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, CityInfoContext cityInfoContext)
         {
             loggerFactory.AddConsole();
 
@@ -76,6 +76,8 @@ namespace CityInfo
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            cityInfoContext.EnsureSeedDataForContext();
 
             app.UseStatusCodePages();
 
